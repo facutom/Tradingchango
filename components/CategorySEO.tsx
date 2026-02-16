c1e8d84ec466ab565a73cb869a93dee627e37f9b
Facu Tom <facu_tomasella@hotmail.com>
2026-02-15 21:28:37 -0300

    pp

--- components/CategorySEO.tsx
+++ components/CategorySEO.tsx
@@ -252,7 +252,7 @@ const CategorySEO: React.FC<CategorySEOProps> = ({ data, categoryName, products 
               </button>
               {activeTooltip === 'dispersion' && (
                 <div className={`${tooltipStyle} left-1/2 -translate-x-1/2`}>
-                  Promedio de las diferencias de precio unitarias entre las distintas cadenas para esta categoría
+                  Promedio de las diferencias de precios unitarias entre las distintas cadenas para esta categoría
                   <div className="absolute top-full left-1/2 -translate-x-1/2 border-4 border-transparent border-t-black/80"></div>
                 </div>
               )}
